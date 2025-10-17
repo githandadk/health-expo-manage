c34ff93c66e1adf7ada203f878ab01baf1b9d167
DavidK <dhkim@andrews.edu>
2025-10-16 20:02:49 -0400

    deps installed

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RegistrationPage from '@/pages/RegistrationPage'
 import LoginPage from '@/pages/admin/LoginPage'
-import AdminIndex from '@/pages/admin'
+import AdminPage from '@/features/admin/AdminPage'
 import LabelPage from '@/features/admin/LabelPage'
 import './index.css'
 
 const router = createBrowserRouter([
   { path: '/', element: <RegistrationPage /> },
   { path: '/admin/login', element: <LoginPage /> },
-  { path: '/admin', element: <AdminIndex /> },
+  { path: '/admin', element: <AdminPage /> },
   { path: '/admin/label/:id', element: <LabelPage /> }
 ])
 
